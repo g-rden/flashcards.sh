dafa4d4f282d4df35f9195f06698e6e2a2198932
g-rden <94605617+g-rden@users.noreply.github.com>
2021-12-13 14:19:25 +0100

    Update flashcards.sh

--- flashcards.sh
+++ flashcards.sh
@@ -1,14 +1,65 @@
 #!/bin/sh
 #
-#	./flashcards.sh [OPTION]... [FILE]...
-#	OPTIONS: 1 normal | 2 show 2nd | ... | 0 show all
+#	./flashcards.sh [OPTION]... [FILE]
+#		OPTIONS: (without spaces!!!)
+#		mode: -m1 normal | -m2 show 2nd | ... | -m0 show all
+#		cards: -c[INTEGER] - how many cards to print
+#		CONTROLS:
+#		next page with ENTER
+#		save and exit with q ENTER, exit with control-C
 
-line=1; voc="$(shuf "$2")"; total="$(awk 'END{print NR}' "$2")"
+showhelp() {
+	awk 'NR>=3 && NR<=9' "$0"; exit
+}
 
-while [ "$line" -le "$total" ]; do
-	clear; printf "%s$line/%s$total\n"
-	echo "$voc" | awk -v l="$line" -v f="$1" -F'=' '{if(NR==l)print $f}'
-	read -r REPLY; clear; printf "%s$line/%s$total\n"
-	echo "$voc" | awk -v l="$line" 'NR==l'
-	read -r REPLY; line=$((line+1))
+if [ -z "$1" ]; then showhelp; fi
+if [ "$#" -gt "3" ]; then printf "Too many arguments\n\n"; showhelp; fi
+
+m="1"
+c="0"
+
+for a in "$@"; do
+	case "$a" in
+		-h | --help) showhelp;;
+		-m*)         m="$(echo "$a" | awk '{print substr($1, 3)}')";;
+		-c*)         c="$(echo "$a" | awk '{print substr($1, 3)}')";;
+		*) if [ -e "$a" ]; then file="$a"; else echo "No such file"; exit; fi;;
+	esac
 done
+
+declarenew() {
+	card="1"; voc="$(shuf "$file")"; total="$(awk 'END{print NR}' "$file")"
+	if [ "$c" -gt 0 ] && [ "$c" -lt "$total" ]; then total="$c"; fi
+}
+
+tmpfile="/$(mktemp -u | awk -F'/' 'BEGIN{OFS=FS} {$NF=""; print}')/$file-tmp"
+
+if [ -e "$tmpfile" ] && [ "$(awk 'NR<=2' "$tmpfile")" = "$(printf "%s$m\n%s$c")" ]; then
+	printf "%s$(awk 'NR==3' "$tmpfile")/$(awk 'NR==4' "$tmpfile")\ncontinue (y) or start a new one (n) "
+	read -r REPLY
+	if [ "$REPLY" = "y" ]; then
+		card="$(awk 'NR==3' "$tmpfile")"
+		total="$(awk 'NR==4' "$tmpfile")"
+		voc="$(awk 'NR>=5' "$tmpfile")"
+	else declarenew
+	fi
+else declarenew
+fi
+
+savequit() {
+	printf "%s$m\n%s$c\n%s$card\n%s$total\n%s$voc" > "$tmpfile"
+	exit
+}
+
+while [ "$card" -le "$total" ]; do
+	if [ "$m" -ne "0" ]; then
+		clear; echo "$card/$total"; echo "$voc" | \
+		awk -v k="$card" -v m="$m" -F'= ' '{if(NR==k) {print $m}}'
+		read -r REPLY; if [ "$REPLY" = "q" ]; then savequit; fi
+	fi
+	clear; printf "%s$card/%s$total\n%s$voc" | \
+	awk -v k="$((card+1))" 'NR==1 || NR==k'
+	read -r REPLY; card=$((card+1)); if [ "$REPLY" = "q" ]; then savequit; fi
+done
+
+if [ -e "$tmpfile" ]; then rm "$tmpfile"; fi
