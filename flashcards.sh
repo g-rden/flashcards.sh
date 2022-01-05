edf8aa196552d8d42cfeede714c51ced4dcd54c6
g-rden <94605617+g-rden@users.noreply.github.com>
2022-01-05 14:20:29 +0100

    Update flashcards.sh

--- flashcards.sh
+++ flashcards.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # flashcards.sh
-# a POSIX complient and almost fully POSIX shell flashcards learning script
+# a POSIX shell flashcards learning script
 #
 
 # USAGE:
@@ -15,34 +15,46 @@
 #    next page with ENTER
 #    save and exit with q ENTER, exit with control-C
 # INPUT FILE REQUIREMENTS:
+#    separate pages with ' = '
 #    last line is an empty new line
 #    no other empty or unused lines
 
 # supress errors
 exec 2>/dev/null
 
+# supress errors
+exec 2>/dev/null
+
 # show help
-! (: "${1:?}" ) && n=1 && while [ "$n" -le 19 ]; do read -r l
+! (: "${1:?}" ) && n=1 && while [ "$n" -le 20 ]; do read -r l
 	[ "$n" -ge 7 ] && a="$a\n$l"; n=$((n+1)); done < "$0" && printf '%s'"$a\n" && exit
 
-# file doesn't exist
+# print error and exit
 if [ -e "$1" ]; then i="$1"
 else printf '%s\n' "$0: cannot access '$1': No such file"; exit; fi
 
-# parsed arguments
+# set variables
 ! (: "${t:?}" ) && t=0 && m="${m:-1}" || m="${m:-2}"; mc="$t" p="${p:-0}"
 
 # for greater enviornment support e.g. termux
-[ "$p" = 1 ] && savefile='tmp-'"${i##*/}" || savefile="${TMPDIR:-/tmp}"'/tmp-'"${i##*/}"
+[ "$p" = 1 ] && savefile='flcrd-'"${i##*/}" || savefile="${TMPDIR:-/tmp}"'/flcrd-'"${i##*/}"
 
-# declare variables if not loaded from a savefile
+# set variables if not loaded from a savefile
 declarenew() {
-	card=1 crt=0 n=0 voc="$(shuf "$i")" # shuf is here
-	# AFAIK it is not possible to get random/pseudo random numbers in POSIX shell.
-	# if it is somehow possible please let me know.
-	# for now it's shuf. if it is not available change it to 'sort -R' or something else.
-	# in the worst case compile one of the many shuf clones from the web.
+	card=1 crt=0 n=0
+	# afaik it isn't possible to get random/pseudo random numbers in POSIX shell.
+	# if it is, please let me know.
+	# the next best thing is awk. still POSIX, but not built into POSIX shell.
+	# i am not interested in awk, so i copy-pasted the code from
+	# https://howto.lintel.in/shuffle-lines-file-linux/
+	# if this is insufficient use your favorite shuffle program instead
+	voc="$(awk 'BEGIN{srand() } { lines[++d]=$0 }
+	END{ while (e<=d){ if (e==d) {break}; RANDOM = int(1 + rand() * d)
+			if ( RANDOM in lines  ){ print lines[RANDOM]; delete lines[RANDOM]
+				++e } } }' "$i")"
+	# show error and exit
 	! (: "${voc:?}" ) && printf "cannot execute shuf! install shuf or replace it in code. sry\n" && exit
+	# determin amount of lines of the input file
 	while read -r line; do n=$((n+1)); done < "$i";	total="${c:-$n}"
 }
 
@@ -56,10 +68,10 @@ if read -r line < "$savefile" && [ "$line" = "$m|$c" ]; then
 $l";;
 		esac; n=$((n+1))
 	done < "$savefile"
-	# remove first line of $voc, which is just a newline
+	# remove first line of $voc, which is just a newline. \n doesn't work
 	voc="${voc#*
 }"; printf '%s'"$card/$total\nresume (*) or not (n): "
-	read -r REPLY; [ "$REPLY" = n ] && declarenew
+	read -r reply; [ "$reply" = n ] && declarenew
 else declarenew; fi
 
 # print selected page
@@ -97,6 +109,8 @@ if [ "$t" -ge 1 ]; then # typing check
 		done
 		# compare typed input with page
 		[ "$REPLY" = "${part%% = *}" ] &&
+			# printing colours from
+			# https://github.com/6gk/fet.sh
 			# 2 is green, 1 is red
 			printf '\033[9%sm\ncorrect\033[0m' 2 && crt=$((crt+1)) ||
 			printf '\033[9%sm\nincorrect\033[0m' 1; printnext; done
@@ -107,9 +121,11 @@ else # no typing check
 		line="${voc%%
 *}"
 		[ "$m" -gt 0 ] && printsel
+		# clearing screen
+		# https://www.climagic.org/mirrors/VT100_Escape_Codes.html
 		printf '\033[2J\033[H%s' "$card/$total"; printnext
 	done
 fi
 
-# delete savefile
-[ -e "$savefile" ] && rm "$savefile"
+# delete savefile or if not possible empty file
+[ -e "$savefile" ] && rmddd "$savefile" || printf '' > "$savefile"
